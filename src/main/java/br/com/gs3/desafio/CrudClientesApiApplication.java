f96c7aaa7078d8026ddc0cffb79b40f2e5d9178c
Luan da Costa <luancostainfo@gmail.com>
2022-01-08 19:46:36 -0300

    Commit inicial

---
+++ src/main/java/br/com/gs3/desafio/CrudClientesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gs3.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClientesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudClientesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gs3/desafio/CrudClientesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gs3.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
