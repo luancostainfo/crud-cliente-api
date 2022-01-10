f49150c6e0a99675a16a157136d4fbb0e08b6b9f
Luan da Costa <luancostainfo@gmail.com>
2022-01-10 08:07:02 -0300

    Configuração de CORS

--- src/main/java/br/com/gs3/desafio/api/controller/ClienteController.java
+++ src/main/java/br/com/gs3/desafio/api/controller/ClienteController.java
@@ -14,6 +14,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
+@CrossOrigin(origins = {"${settings.cors_origin}"})
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
